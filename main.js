d06aaf384050fe32897f64f25b146d40b8becbda
ayu01 <allen.yu@oath.com>
2021-02-13 02:32:30 +0800

    adjust height

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ function clickHandler() {
   var coupon = document.querySelector('.coupon');
   coupon.classList.toggle("show");
 }
+var vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  var vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
